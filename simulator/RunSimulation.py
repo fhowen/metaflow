ae9e89413b4331ac87ec8aedc886d2368d2c85dc
Phlix <819108840@qq.com>
2018-12-26 10:30:49 +0800

    Update RunSimulation.py

--- simulator/RunSimulation.py
+++ simulator/RunSimulation.py
@@ -4,12 +4,13 @@ from Compu import Compu
 from Reducer import Reducer
 from Job import Job
 from JobSet import JobSet
-
-
+from Simulator import Simulator
 
 
 js = JobSet()
 js.readTrace()
-print(Flow.TotalFlowNum)
+#print(Flow.TotalFlowNum)
 js.genDag()
-print(Flow.TotalFlowNum)
+#print(Flow.TotalFlowNum)
+simu = Simulator(js)
+simu.simulate(1000)
