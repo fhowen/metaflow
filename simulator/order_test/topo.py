15c2c06536ae95b597a5cc5ad92d9137ccbdae7c
shiy10 <shiyang7@qq.com>
2019-01-31 13:16:51 +0800

    order_test

---
+++ simulator/order_test/topo.py
@@ -0,0 +1,78 @@
+# represent 4,5,6,7
+node_list = [0,1,2,3]
+# represent edges 
+'''
+direct edges
+   0  1  2  3
+ ------------
+0 |0        
+1 |   0    
+2 |      0  
+3 |         0
+in total , 12 variables
+'''
+edge_matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+def topo_num():
+    topoNum= 0
+    return topoNum
+
+def judge_loop():
+    return False
+
+def save_to_file(num):
+    pass
+
+
+
+def generate_dag():
+    i = 0
+    for a_1 in range(0,2):
+        edge_matrix[0][1] = a_1
+        for a_2 in range(0,2):
+            edge_matrix[0][2] = a_2
+            for a_3 in range(0,2):
+                edge_matrix[0][3] = a_3
+                for a_4 in range(0,2):
+                    edge_matrix[1][0] = a_4
+                    if edge_matrix[1][0] + edge_matrix[0][1] == 2:
+                        break
+                    for a_5 in range(0,2):
+                        edge_matrix[1][2] = a_5
+                        for a_6 in range(0,2):
+                            edge_matrix[1][3] = a_6
+                            for a_7 in range(0,2):
+                                edge_matrix[2][0] = a_7
+                                if edge_matrix[2][0] + edge_matrix[0][2] == 2:
+                                    break
+                                for a_8 in range(0,2):
+                                    edge_matrix[2][1] = a_8
+                                    if edge_matrix[2][1] + edge_matrix[1][2] == 2:
+                                        break
+                                    for a_9 in range(0,2):
+                                        edge_matrix[2][3] = a_9
+                                        for a_10 in range(0,2):
+                                            edge_matrix[3][0] = a_10
+                                            if edge_matrix[3][0] + edge_matrix[0][3] == 2:
+                                                break
+                                            for a_11 in range(0,2):
+                                                edge_matrix[3][1] = a_11
+                                                if edge_matrix[3][1] + edge_matrix[1][3] == 2:
+                                                    break
+                                                for a_12 in range(0,2):
+                                                    edge_matrix[3][2] = a_12
+                                                    if edge_matrix[3][2] + edge_matrix[2][3] == 2:
+                                                        break
+                                                    i += 1
+                                                    has_loop = judge_loop()
+                                                    if has_loop:
+                                                        continue
+                                                    else:
+                                                        num = topo_num()
+                                                        save_to_file(num)
+    print(i)
+
+
+if __name__ == '__main__':
+    generate_dag()
+
+
